42d3689bddef28165b19da19336ae3b9db951d02
elmehdiayad <ay.elmehdi@gmail.com>
2024-08-18 16:45:31 +0100

    use netlify form

--- src/components/common/ContactDialog.vue
+++ src/components/common/ContactDialog.vue
@@ -1,61 +1,43 @@
 <template>
   <Transition name="fade">
-    <div
-      v-show="$show"
+    <div v-show="$show"
       class="bg-dark-blur z-1000 dialog pointer-events-auto fixed inset-0 grid w-full cursor-pointer place-items-center"
-      @click="hide()"
-    >
+      @click="hide()">
       <div @click.stop class="container-md relative">
-        <div
-          class="surface-base dialog__inner dialog-grid relative overflow-hidden rounded-2xl shadow-xl"
-        >
+        <div class="surface-base dialog__inner dialog-grid relative overflow-hidden rounded-2xl shadow-xl">
           <div class="overflow-hidden md:block">
             <slot name="image" />
           </div>
-          <div
-            class="hide-scrollbar dialog__content relative overflow-hidden p-8 md:p-14"
-          >
-            <form @submit.prevent="submit" class="grid gap-8">
+          <div class="hide-scrollbar dialog__content relative overflow-hidden p-8 md:p-14">
+            <form @submit.prevent="submit" class="grid gap-8" data-netlify="true" name="contact" method="POST"
+              netlify-honeypot="bot-field">
+              <input type="hidden" name="form-name" value="contact" />
+              <p class="hidden">
+                <label>
+                  Don't fill this out if you're human: <input name="bot-field" />
+                </label>
+              </p>
+
               <div class="grid gap-4 pb-8">
                 <h2 class="title-sm">{{ contact?.title }}</h2>
 
                 <slot name="content" />
               </div>
-              <div
-                class="input-group z-20 w-full"
-                v-if="contact.topics.length > 1"
-              >
-                <Popper
-                  placement="bottom-start"
-                  offsetDistance="1"
-                  :show="showPopper"
-                  class="w-full"
-                >
-                  <button
-                    type="button"
-                    @click="showPopper = !showPopper"
-                    class="select surface-overlay w-full text-left"
-                  >
+              <div class="input-group z-20 w-full" v-if="contact.topics.length > 1">
+                <Popper placement="bottom-start" offsetDistance="1" :show="showPopper" class="w-full">
+                  <button type="button" @click="showPopper = !showPopper"
+                    class="select surface-overlay w-full text-left">
                     {{ !!topic ? topic : "Select" }}
                   </button>
 
                   <template #content>
                     <ul>
-                      <li
-                        v-for="(item, index) in contact.topics"
-                        :key="index"
-                        :class="
-                          topic == item.label ? 'bg-dark bg-opacity-10' : ''
-                        "
-                      >
-                        <button
-                          type="button"
-                          class="w-full p-2 text-left hover:bg-dark hover:bg-opacity-10"
-                          @click="
-                            setTopic(item);
-                            showPopper = false;
-                          "
-                        >
+                      <li v-for="(item, index) in contact.topics" :key="index" :class="topic == item.label ? 'bg-dark bg-opacity-10' : ''
+                        ">
+                        <button type="button" class="w-full p-2 text-left hover:bg-dark hover:bg-opacity-10" @click="
+                          setTopic(item);
+                        showPopper = false;
+                        ">
                           {{ item.label }}
                         </button>
                       </li>
@@ -64,74 +46,38 @@
                 </Popper>
 
                 <label
-                  class="peer-placeholder-shown:left-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:text-primary"
-                  >{{ t("topic") }} *</label
-                >
+                  class="peer-placeholder-shown:left-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:text-primary">{{
+                    t("topic") }} *</label>
               </div>
+              <input type="hidden" name="topic" :value="topic" />
               <div class="input-group">
-                <input
-                  type="text"
-                  name="name"
-                  placeholder=" "
-                  class="surface-overlay peer"
-                  v-model="form.name"
-                />
+                <input type="text" name="name" placeholder=" " class="surface-overlay peer" v-model="form.name" />
                 <label
-                  class="peer-placeholder-shown:left-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:text-primary"
-                  >{{ t("name") }} *</label
-                >
+                  class="peer-placeholder-shown:left-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:text-primary">{{
+                    t("name") }} *</label>
               </div>
 
               <div class="input-group">
-                <input
-                  type="email"
-                  name="email"
-                  placeholder=" "
-                  class="surface-overlay peer"
-                  v-model="form.email"
-                />
+                <input type="email" name="email" placeholder=" " class="surface-overlay peer" v-model="form.email" />
                 <label
-                  class="peer-placeholder-shown:left-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:text-primary"
-                  >{{ t("email") }} *</label
-                >
+                  class="peer-placeholder-shown:left-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:text-primary">{{
+                    t("email") }} *</label>
               </div>
               <div class="input-group">
-                <input
-                  type="text"
-                  name="phone"
-                  placeholder=" "
-                  class="surface-overlay peer"
-                  v-model="form.phone"
-                />
+                <input type="text" name="phone" placeholder=" " class="surface-overlay peer" v-model="form.phone" />
                 <label
-                  class="peer-placeholder-shown:left-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:text-primary"
-                  >{{ t("phone") }}</label
-                >
+                  class="peer-placeholder-shown:left-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:text-primary">{{
+                    t("phone") }}</label>
               </div>
               <div class="input-group">
-                <textarea
-                  class="surface-overlay peer"
-                  name="message"
-                  id=""
-                  placeholder=" "
-                  cols="30"
-                  rows="2"
-                  ref="textarea"
-                  v-model="input"
-                ></textarea>
+                <textarea class="surface-overlay peer" name="message" id="" placeholder=" " cols="30" rows="2"
+                  ref="textarea" v-model="input"></textarea>
                 <label
-                  class="peer-placeholder-shown:left-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:text-primary"
-                  >Message *</label
-                >
+                  class="peer-placeholder-shown:left-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:text-primary">Message
+                  *</label>
               </div>
-              <div
-                class="pointer-events-none right-5 mb-14 flex translate-y-10 justify-end md:sticky md:bottom-0"
-              >
-                <button
-                  class="btn surface-primary pointer-events-auto"
-                  type="submit"
-                  :disabled="!canSubmit"
-                >
+              <div class="pointer-events-none right-5 mb-14 flex translate-y-10 justify-end md:sticky md:bottom-0">
+                <button class="btn surface-primary pointer-events-auto" type="submit" :disabled="!canSubmit">
                   {{ t("submit") }}
                 </button>
               </div>
@@ -139,10 +85,7 @@
             </form>
           </div>
         </div>
-        <button
-          class="btn btn-icon surface-dark btn-absolute -right-3 -top-3 z-10"
-          @click="hide()"
-        >
+        <button class="btn btn-icon surface-dark btn-absolute -right-3 -top-3 z-10" @click="hide()">
           <slot />
         </button>
       </div>
@@ -201,7 +144,7 @@ const rules = {
 };
 const { pass, isFinished, errorFields } = useAsyncValidator(form, rules);
 
-onMounted(() => {});
+onMounted(() => { });
 
 const topic = ref(null);
 const showPopper = ref(false);
@@ -214,7 +157,7 @@ const hide = () => {
 };
 
 const setTopic = (data) => {
-  
+
   topic.value = data.label;
   topicEmail.value = data.email;
   topicChannel.value = data.slack_id;
@@ -239,47 +182,54 @@ Topic:  ${topic.value}\r\n
 Name: ${form.name}\r\n
 Phone: ${form.phone}\r\n
 Email: ${form.email}\r\n
-Message: \r\n${form.message}\r\n           `,
+Message: \r\n${form.message}\r\n`,
   };
 });
 
+const resetForm = () => {
+  // Reset the reactive form object
+  form.email = "";
+  form.name = "";
+  form.phone = "";
+  form.message = "";
+  // Reset the input ref (for the textarea)
+  input.value = "";
+
+};
+
 const submit = () => {
-  loading.value = true; 
-  if (!!props.contact.provider) {
-    fetch(`/api/contact-${props.contact.provider}`, {
-      method: "POST",
-      body: JSON.stringify(mailData.value),
-      headers: { "Content-Type": "application/json" },
+  loading.value = true;
+  const formElement = document.querySelector('form[name="contact"]');
+  const formData = new FormData(formElement);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then((response) => {
+      if (response.ok) {
+        toast.success(t("contact_thanks"));
+        resetForm();
+        hide();
+      } else {
+        throw new Error("Network response was not ok");
+      }
     })
-      .then((r) => r.json())
-      .then((data) => {
-        if (data.status === "ok") {
-          toast.success(t("contact_thanks"));
-          form.email = "";
-          form.name = "";
-          form.phone = "";
-          form.message = "";
-          input.value = "";
-          hide();
-        } else {
-          toast.error(t("contact_error"));
-        }
-      })
-      .catch((e) => {
-        console.log("error", e);
-        toast.error(t("contact_error"));
-      })
-      .finally(() => {
-        loading.value = false;
-      });
-  }
+    .catch((error) => {
+      console.error("Error:", error);
+      toast.error(t("contact_error"));
+    })
+    .finally(() => {
+      loading.value = false;
+    });
 };
 
 watch(
   $show,
 
   (val) => {
-    if (val) { 
+    if (val) {
       disableBodyScroll(document.body);
     } else {
       enableBodyScroll(document.body);
@@ -302,8 +252,10 @@ watch(
 .z-1000 {
   z-index: 1000;
 }
+
 .dialog-grid {
   @apply grid grid-cols-1;
+
   @screen md {
     grid-template-columns: 4fr 5fr;
   }
@@ -315,10 +267,12 @@ watch(
 
 .dialog {
   --popper-theme-padding: 0;
+
   &__inner {
     max-height: calc(100vh - 2rem);
     overflow-x: hidden;
     overflow-y: auto;
+
     @screen md {
       height: min(100vh - 2rem, 40rem);
     }
@@ -337,13 +291,15 @@ watch(
 
 .input-group {
   @apply relative isolate;
+
   input,
   textarea,
   .select {
-    @apply block w-full rounded-2xl px-3 py-2.5  focus:outline-primary;
+    @apply block w-full rounded-2xl px-3 py-2.5 focus:outline-primary;
   }
+
   label {
-    @apply pointer-events-none absolute left-0 top-3 z-10 origin-[0] -translate-y-9  scale-75 transform text-sm  duration-300;
+    @apply pointer-events-none absolute left-0 top-3 z-10 origin-[0] -translate-y-9 scale-75 transform text-sm duration-300;
   }
 }
 </style>
--- src/components/newsletter/Newsletter.vue
+++ src/components/newsletter/Newsletter.vue
@@ -1,22 +1,17 @@
 <template>
-  <form
-    name="newsletter-subscribes"
-    class="relative inline-flex items-center gap-4 py-4"
-    @submit.prevent="submit"
-  >
-    <input
-      type="email"
-      :placeholder="t('email')"
-      v-model="form.email"
+  <form name="newsletter-subscribes" class="relative inline-flex items-center gap-4 py-4" @submit.prevent="submit"
+    data-netlify="true" method="POST" netlify-honeypot="bot-field">
+    <input type="hidden" name="form-name" value="newsletter-subscribes" />
+    <p class="hidden">
+      <label>
+        Don't fill this out if you're human: <input name="bot-field" />
+      </label>
+    </p>
+    <input type="email" name="email" :placeholder="t('email')" v-model="form.email"
       class="relative block overflow-hidden rounded-full bg-dark px-5 py-2"
-      :class="errorFields?.email?.length ? 'text-warning' : 'text-light'"
-    />
-    <button
-      type="submit"
-      :disabled="!canSubmit"
-      class="btn"
-      :class="canSubmit ? 'surface-primary' : 'surface-base opacity-50'"
-    >
+      :class="errorFields?.email?.length ? 'text-warning' : 'text-light'" />
+    <button type="submit" :disabled="!canSubmit" class="btn"
+      :class="canSubmit ? 'surface-primary' : 'surface-base opacity-50'">
       {{ t("subscribe") }}
     </button>
     <Loading :loading="loading" />
@@ -30,17 +25,20 @@ import { useAsyncValidator } from "@vueuse/integrations/useAsyncValidator";
 import Loading from "@components/common/Loading.vue";
 import "vue3-toastify/dist/index.css";
 import { toast } from "vue3-toastify";
+
 const props = defineProps({
   type: {
     type: String,
     required: false,
-    default: "mailchimp",
+    default: "netlify",
   },
   list_id: String,
 });
+
 const loading = ref(false);
 const message = ref(null);
 const form = reactive({ email: "" });
+
 const rules = {
   email: [
     {
@@ -49,38 +47,41 @@ const rules = {
     },
   ],
 };
+
 const { pass, isFinished, errorFields } = useAsyncValidator(form, rules);
+
 const canSubmit = computed(() => {
   return !loading.value && isFinished.value && pass.value;
 });
 
+const resetForm = () => {
+  form.email = "";
+};
+
 const submit = () => {
-  if (props.type === "mailchimp") {
-    loading.value = true;
-    fetch("/api/subscribe-mailchimp", {
-      method: "POST",
-      body: JSON.stringify({ email: form.email }),
-      headers: { "Content-Type": "application/json" },
+  loading.value = true;
+  const formElement = document.querySelector('form[name="newsletter-subscribes"]');
+  const formData = new FormData(formElement);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then((response) => {
+      if (response.ok) {
+        toast.success(t("newsletter_thanks"));
+        resetForm();
+      } else {
+        throw new Error("Network response was not ok");
+      }
     })
-      .then((r) => r.json())
-      .then((data) => {
-        if (data.status === "pending") {
-          toast.success(t("newsletter_thanks"));
-          form.email = "";
-        } else if (data.status === "Member Exists") {
-          toast.info(t("newsletter_already_subscribed"));
-          form.email = "";
-        } else {
-          toast.error(t("newsletter_error"));
-        }
-      })
-      .catch((e) => {
-        message.value = t("newsletter_error");
-        toast.error(t("newsletter_error"));
-      })
-      .finally(() => {
-        loading.value = false;
-      });
-  }
+    .catch((error) => {
+      console.error("Error:", error);
+      toast.error(t("newsletter_error"));
+    })
+    .finally(() => {
+      loading.value = false;
+    });
 };
 </script>
